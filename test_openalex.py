04dbffedb55211ca660b3dcc1f69f2d6a309146e
ShardulMore112 <shardulmore112@gmail.com>
2025-08-10 15:28:31 +0530

    R

--- test_openalex.py
+++
@@ -1,7 +0,0 @@
-from tools import openalex_tool
-
-if __name__ == "__main__":
-    query = input("Enter research field or keyword: ").strip()
-    result = openalex_tool.func(query)
-    print("\n--- Results ---\n")
-    print(result)
